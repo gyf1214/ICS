af4f4d0e9583b238e0163cc28958d7bbc884b549
gyf1214 <gyf1214@gmail.com>
2016-11-02 18:20:16 +0800

    first ver

--- LAB-5/trans.c
+++ LAB-5/trans.c
@@ -2,13 +2,9 @@
 #include "cachelab.h"
 
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
-}
 
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
+void trans(int M, int N, int A[N][M], int B[M][N]) {
     int i, j, tmp;
 
     for (i = 0; i < N; i++) {
@@ -19,9 +15,26 @@ void trans(int M, int N, int A[N][M], int B[M][N])
     }
 }
 
+#define BLOCK 256
+
+char trans1_desc[] = "Ver 1.0";
+void trans1(int M, int N, int A[N][M], int B[M][N]) {
+    int size = BLOCK / N;
+    int bm = M / size, bn = N / size;
+    int i, j, bi, bj;
+    for (bi = 0; bi < bn; ++bi) {
+        for (bj = 0; bj < bm; ++bj) {
+            for (i = bi * size; i < bi * size + size; ++i) {
+                for (j = bj * size; j < bj * size + size; ++j) {
+                    A[i][j] = B[j][i];
+                }
+            }
+        }
+    }
+}
+
 void registerFunctions()
 {
-    registerTransFunction(transpose_submit, transpose_submit_desc);
     registerTransFunction(trans, trans_desc);
-
+    registerTransFunction(trans1, trans1_desc);
 }
