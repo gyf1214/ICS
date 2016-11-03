4fc4757041fc9832665ec2f09fef36b93ba3a88b
gyf1214 <gyf1214@gmail.com>
2016-11-03 10:58:32 +0800

    start 64x64

--- LAB-5/trans.c
+++ LAB-5/trans.c
@@ -26,8 +26,8 @@ void transBlock(int M, int N, int A[N][M], int B[M][N], int bi, int bj, int bloc
     }
 }
 
-char trans1_desc[] = "Ver 1.0";
-void trans1(int M, int N, int A[N][M], int B[M][N]) {
+char desc32[] = "For 32x32";
+void trans32(int M, int N, int A[N][M], int B[M][N]) {
     int i, j, k;
     for (k = BLOCK; k < N; k += BLOCK) {
         for (i = 0; i < BLOCK; ++i) {
@@ -55,8 +55,21 @@ void trans1(int M, int N, int A[N][M], int B[M][N]) {
     }
 }
 
+#define BLOCK2 4
+
+char desc64[] = "For 64x64";
+void trans64(int M, int N, int A[N][M], int B[M][N]) {
+    int i, j;
+    for (i = 0; i < N; i += BLOCK2) {
+        for (j = 0; j < M; j += BLOCK2) {
+            transBlock(M, N, A, B, i, j, BLOCK2);
+        }
+    }
+}
+
 void registerFunctions()
 {
     registerTransFunction(trans, trans_desc);
-    registerTransFunction(trans1, trans1_desc);
+    registerTransFunction(trans32, desc32);
+    registerTransFunction(trans64, desc64);
 }
