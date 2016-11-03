b902e4a2141eaacfc1a6c0e052cb096e5cd35a3b
gyf1214 <gyf1214@gmail.com>
2016-11-03 13:14:11 +0800

    add tsim to debug & solve bug in trans64

--- LAB-5/trans.c
+++ LAB-5/trans.c
@@ -43,9 +43,9 @@ void transHalf(int M, int N, int A[N][M], int B[M][N], int bi, int bj, int t) {
     int i, j;
     repHALF(i, j, bi, bj) B[j][i] = A[i][j];
     repHALF(i, j, 0, 0) B[t + j][t + i] = A[i + bi][j + bj + HALF];
-    repHALF(i, j, bi + HALF / 2, bj) B[j][i] = A[i][j];
+    repHALF(i, j, bi + HALF, bj) B[j][i] = A[i][j];
     repHALF(i, j, 0, 0) B[j + bj + HALF][i + bi] = B[t + j][t + i];
-    repHALF(i, j, bi + HALF / 2, bj + HALF / 2) B[j][i] = A[i][j];
+    repHALF(i, j, bi + HALF, bj + HALF) B[j][i] = A[i][j];
 }
 
 char desc64[] = "For 64x64";
@@ -53,9 +53,9 @@ void trans64(int M, int N, int A[N][M], int B[M][N]) {
     int i, j, k;
     iteBLOCK(i, 1, N) iteBLOCK(j, 1, M) if (i != j) transHalf(M, N, A, B, i, j, 0);
     iteBLOCK(i, 2, N) transHalf(M, N, A, B, i, 0, 1);
-    iteBLOCK(j, 2, N) transHalf(M, N, A, B, 0, j, 1);
-    transHalf(M, N, A, B, 1, 0, 2);
-    transHalf(M, N, A, B, 0, 1, 2);
+    iteBLOCK(j, 2, M) transHalf(M, N, A, B, 0, j, 1);
+    transHalf(M, N, A, B, BLOCK, 0, 2);
+    transHalf(M, N, A, B, 0, BLOCK, 2);
     iteBLOCK(k, 0, N) {
         repBLOCK(i, j, k, k) B[j][i] = A[i][j];
     }
---
+++ LAB-5/tsim.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "cachelab.h"
+
+#define SIZE 32
+#define rep(i) for (i = 0; i < SIZE; ++i)
+
+void trans64(int M, int N, int A[N][M], int B[M][N]);
+
+int A[SIZE][SIZE], B[SIZE][SIZE];
+
+void registerTransFunction(void (*trans)(int M,int N,int[N][M],int[M][N]),
+                           char* desc) {
+
+                           }
+
+void print(int A[][SIZE]) {
+    int i, j;
+    rep(i) rep(j) {
+        printf("%d%c", A[i][j], j == SIZE - 1 ? '\n' : '\t');
+    }
+}
+
+int main() {
+    int i, j;
+    rep(i) rep(j) {
+        A[i][j] = i * SIZE + j;
+    }
+    print(A);
+    trans64(SIZE, SIZE, A, B);
+    print(B);
+
+    return 0;
+}
