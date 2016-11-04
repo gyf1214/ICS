bd5d867d92d467a256d560634aa0def42ae04987
gyf1214 <gyf1214@gmail.com>
2016-11-04 09:37:35 +0800

    delete useless function

--- LAB-5/trans.c
+++ LAB-5/trans.c
@@ -3,18 +3,6 @@
 
 char transpose_submit_desc[] = "Transpose submission";
 
-char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N]) {
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }
-}
-
 #define BLOCK (8)
 #define HALF (BLOCK / 2)
 #define repBLOCK(bi, bj) for (i = (bi); i < (bi) + BLOCK; ++i) for (j = (bj); j < (bj) + BLOCK; ++j)
