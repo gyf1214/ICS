84937ce35ea51bca1677f714e16bbae7e37e0223
gyf1214 <gyf1214@gmail.com>
2016-11-21 14:46:35 +0800

    test

--- LAB-6/src/tsh.c
+++ LAB-6/src/tsh.c
@@ -158,8 +158,7 @@ void runProg(char *cmdline, struct cmdline_tokens *tok, int bg) {
     }
 
     struct job_t *job = addjob(job_list, pid, bg + 1, cmdline);
-    // printJob(job);
-    if (bg != 0) printJob(job);
+    if (bg) printJob(job);
 
     setMask(SIG_UNBLOCK);
 
