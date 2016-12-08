705acf036b3b4acb76e43aef12ca92ff45589325
gyf1214 <gyf1214@gmail.com>
2016-12-08 21:19:58 +0800

    change CHUNKSIZE

--- LAB-7/mm-segregated.c
+++ LAB-7/mm-segregated.c
@@ -61,7 +61,7 @@ typedef unsigned long u64, *p64;
 #define NEXT(p)                 (*(pptr)(p))
 #define PREV(p)                 (*((pptr)(p) + 1))
 
-#define CHUNKSIZE               (1 << 10)
+#define CHUNKSIZE               (1 << 8)
 
 #define MINSIZE                 16
 #define LISTCNT                 9
