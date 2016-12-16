4e34d0cf8ecaf8a9ed099bf98d6653a04ae8dfb5
gyf1214 <gyf1214@gmail.com>
2016-12-16 16:29:54 +0800

    noting

--- LAB-7/mm-segregated.c
+++ LAB-7/mm-segregated.c
@@ -55,7 +55,7 @@ typedef unsigned long u64, *p64;
 #define PTR(p)                  ((p) ? (base + (p)) : NULL)
 #define OFF(p)                  ((p) ? ((p) - base) : 0)
 
-#define CHUNKSIZE               (1 << 10)
+#define CHUNKSIZE               ALIGNMENT
 
 #define MINSIZE                 8
 #define LISTCNT                 12
