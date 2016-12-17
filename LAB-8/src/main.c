7d814ae14c91438a2c562376e12305c71a27733b
gyf1214 <gyf1214@gmail.com>
2016-12-18 03:23:25 +0800

    add main

--- LAB-8/src/main.c
+++ LAB-8/src/main.c
@@ -1,12 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include "proxy.h"
+#include "pool.h"
 
 /* Recommended max cache and object sizes */
-#define MAX_CACHE_SIZE 1049000
-#define MAX_OBJECT_SIZE 102400
+// #define MAX_CACHE_SIZE 1049000
+// #define MAX_OBJECT_SIZE 102400
 
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+// static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main() {
-    
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        printf("usage: %s <port>\n", argv[0]);
+        exit(0);
+    }
+
+    initPool();
+    createServer(argv[1]);
+    eventLoop();
+
+    return 0;
 }
